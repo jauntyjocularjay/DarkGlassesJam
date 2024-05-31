d4bf5a2a2ab771bce1e488d5608204e263b02076
jauntyjocularjay <77408854+jauntyjocularjay@users.noreply.github.com>
2024-05-30 19:05:45 -0700

    feature: added separate UI script, updated README with design doc, modules

--- DarkGlasses.mjs
+++ DarkGlasses.mjs
@@ -1,96 +1,11 @@
-import {
-    // Constants
-    display,
-    flex,
-    event,
-    unit,
 
-    // Base Classes
-    Selection,
-    StyleSheet,
-    Listener,
-    ListenerOnLoad,
-    // FlexBoxClass,
 
-    // Classables
-    // // Containers
-    Img,
-    Div,
-    DivBtn,
-    FlexBox,
-    Figure,
-    Form,
-    Label,
-    // // Input
-    Button,
-    Input,
-    TextArea,
-    Select,
-    Option,
-    // // Format elements
-    Br,
-    //  // External Resource Links
-    Link,
-    Style,
+let cheats_enabled = false
 
-    // Text Elements
-    // // Headers
-    H1,
-    H2,
-    H3,
-    H4,
-    H5,
-    H6,
-    // // Body Text
-    P,
-    Figcaption,
-    A,
-    Strong,
-    Abbr,
-    Blockquote,
-    Sub,
-    Sup,
-    Span,
-    Text,
-    Code,
-    Pre,
 
-    // Functions
-    getStylesheetByFileName,
-    addAdoptedStyleSheet,
 
-    // JSONCSS
-    JSONCSS,
-    UnsupportedJSONCSSError,
-    PercentageOutOfRangeError
-} from './vjsc/vanilla.mjs'
-
-const css = {
-    class: {
-        mood0: 'mood0',
-        mood1: 'mood1',
-        mood2: 'mood2',
-        mood3: 'mood3',
-        mood4: 'mood4',
-        mood5: 'mood5',
-        mood6: 'mood6',
-        mood7: 'mood7',
-        mood8: 'mood8',
-        mood9: 'mood9',
-        embossedCard: 'embossedCard',
-        choice: {
-            enabledDark: 'enabledChoiceDark',
-            enabledLight: 'enabledChoiceLight',
-            disabledDark: 'disabledChoiceDark',
-            disabledLight: 'disabledChoiceLight',
-        }
-    },
-    id: {
-
-    },
-    keyframe: {
-
-    }
+function enableCheats(){
+    cheats_enabled = true
 }
 
 export {
---
+++ UserInterface.mjs
@@ -0,0 +1,102 @@
+import {
+    // Constants
+    display,
+    flex,
+    event,
+    unit,
+
+    // Base Classes
+    Selection,
+    StyleSheet,
+    Listener,
+    ListenerOnLoad,
+    // FlexBoxClass,
+
+    // Classables
+    // // Containers
+    Img,
+    Div,
+    DivBtn,
+    FlexBox,
+    Figure,
+    Form,
+    Label,
+    // // Input
+    Button,
+    Input,
+    TextArea,
+    Select,
+    Option,
+    // // Format elements
+    Br,
+    //  // External Resource Links
+    Link,
+    Style,
+
+    // Text Elements
+    // // Headers
+    H1,
+    H2,
+    H3,
+    H4,
+    H5,
+    H6,
+    // // Body Text
+    P,
+    Figcaption,
+    A,
+    Strong,
+    Abbr,
+    Blockquote,
+    Sub,
+    Sup,
+    Span,
+    Text,
+    Code,
+    Pre,
+
+    // Functions
+    getStylesheetByFileName,
+    addAdoptedStyleSheet,
+
+    // JSONCSS
+    JSONCSS,
+    UnsupportedJSONCSSError,
+    PercentageOutOfRangeError
+} from './vjsc/vanilla.mjs'
+
+const css = {
+    class: {
+        mood: [
+            'mood00',
+            'mood01',
+            'mood02',
+            'mood03',
+            'mood04',
+            'mood05',
+            'mood06',
+            'mood07',
+            'mood08',
+            'mood09',
+            'mood10',
+            'mood11',
+            'mood12'
+        ],
+        embossedCard: 'embossedCard',
+        choice: {
+            enabledDark: 'enabledChoiceDark',
+            enabledLight: 'enabledChoiceLight',
+            disabledDark: 'disabledChoiceDark',
+            disabledLight: 'disabledChoiceLight',
+        }
+    },
+    id: {
+
+    },
+    keyframe: { // CSS Animations
+
+    }
+}
+
+
+
